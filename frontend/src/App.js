8d09016a2f11e56acf7fa70d5a16ba0ec87f17ec
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-12-11 13:19:26 +0800

    Fixed axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import './App.scss';
 
 var axios = require('axios');
-var data = JSON.stringify({"username":"Group1","password":"Y8UAsjalJ4wcwYK"});
-
+//var data = JSON.stringify({"username":"Group1","password":"Y8UAsjalJ4wcwYK"});
+var data = JSON.stringify({"custID":1});
 var config = {
   method: 'post',
-  url: 'https://cors-anywhere.herokuapp.com/u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login',
   headers: { 
     'Content-Type': 'application/json', 
     'x-api-key': 'A4DG0b4pmab5dO6Iguc91CFWWnOtpLu84FZtniQb'
@@ -15,16 +14,17 @@ var config = {
 };
 
 
+export async function postView(){
+  console.log("postView()...");
+  const res = await axios.post('https://cors-anywhere.herokuapp.com/https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view',data,config); //post(url,data)
+  console.log("res",res);
+  return res;
+}
 
 class App extends Component {
   login = () =>{
-    axios(config)
-    .then(function (response) {
-      console.log('response',response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    console.log("login()...")
+    postView();
   }
 
   render(){
