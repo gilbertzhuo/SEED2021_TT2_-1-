c9631a4513ad08bbca1b61e0ea62d412a89c5921
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-12-11 12:34:24 +0800

    Cors fixed but error 404 now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,51 @@
+import React, { Component } from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <>
-    <h1>hello world</h1>
-    </>
-  );
+var axios = require('axios');
+var data = JSON.stringify({"username":"Group1","password":"Y8UAsjalJ4wcwYK"});
+
+var config = {
+  method: 'post',
+  url: 'https://cors-anywhere.herokuapp.com/u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login',
+  headers: { 
+    'Content-Type': 'application/json', 
+    'x-api-key': 'A4DG0b4pmab5dO6Iguc91CFWWnOtpLu84FZtniQb'
+  },
+  data : data
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+class App extends Component {
+  login = () =>{
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        <h1>API Login Test</h1>
+        <button onClick = {this.login}
+            className="btn btn-secondary btn-sm"
+          >
+            Magic Login
+          </button>
+      </React.Fragment>
+    );
+  }
+  
 }
 
 export default App;
