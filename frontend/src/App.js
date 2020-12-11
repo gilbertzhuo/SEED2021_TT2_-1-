4f6cf2e7613d6d6d19279fba56fc793792ec1577
byc7 <54060580+byc7@users.noreply.github.com>
2020-12-11 12:39:47 +0800

    Add files via upload
    fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
-import './App.scss';
+import React from 'react';
+import Nav from './Nav';
+import Login from './Login';
+import './index.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-    <h1>hi</h1>
-    </>
-  );
+    return (
+        // <Router> all components under it have routing ability
+        // <Route>: renders component out base on url
+        // if any letter under path matches, will be displayed and continue onto next <Route>
+        // <Switch>: ensures that once a route is accessed the whole search process will stop
+        // exact: putting it in <Route> will ensure render occurs if exact match
+        <Router>
+            <div className="App">
+                <Nav />
+                    <Switch>
+                        <Route path="/" exact component={ Home } />
+                        <Route path="/login" exact component={ Login } />
+                    </Switch>
+            </div>
+        </Router>
+    )    
 }
+
+const Home = () => (
+    <div>    
+        <h1>Home Page</h1>
+    </div>
+);
 
 export default App;
---
+++ frontend/src/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+import {Link} from 'react-router-dom';
+
+function Nav() {
+
+    const navStyle = {
+        color: 'white'
+    }
+
+    return (
+        <nav>
+            <h3>Logo</h3>
+            <ul>
+                <Link style={ navStyle } to="/login">
+                    <li>Login</li>
+                </Link>
+            </ul>
+        </nav>
+    )    
+}
+
+export default Nav;
