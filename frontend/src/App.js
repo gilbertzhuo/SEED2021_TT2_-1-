b5a5dce3d06898102e1f132587415e3f548ecd86
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-12-11 13:54:26 +0800

    Update to display account name
    Not yet formatted properly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.scss';
 
 var axios = require('axios');
-//var data = JSON.stringify({"username":"Group1","password":"Y8UAsjalJ4wcwYK"});
 var data = JSON.stringify({"custID":1});
 var config = {
   method: 'post',
@@ -14,28 +13,35 @@ var config = {
 };
 
 
-export async function postView(){
-  console.log("postView()...");
-  const res = await axios.post('https://cors-anywhere.herokuapp.com/https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view',data,config); //post(url,data)
-  console.log("res.data",res.data);
-  return res.data;
-}
 
 class App extends Component {
-  login = () =>{
-    console.log("login()...")
-    return postView();
+  state={
+    accounts:null,
+    accNames:[],
+    accBal:[]
+  }
+  async postView(){
+    console.log("postView()...");
+    const res = await axios.post('https://cors-anywhere.herokuapp.com/https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view',data,config); //post(url,data)
+    const hold = Object.values(res.data);
+    console.log("hold arr: res.data",hold[0]);
+    this.setState({accounts:Object.values(hold[0])});
+  }
+
+  handleView = () =>{
+    console.log("handleView() receives ",this.postView());
   }
 
   render(){
     return (
       <React.Fragment>
         <h1>API Login Test</h1>
-        <button onClick = {this.login}
+        <button onClick = {this.handleView}
             className="btn btn-secondary btn-sm"
           >
             Magic Login
           </button>
+          <h2>{this.state.accounts}</h2>
       </React.Fragment>
     );
   }
