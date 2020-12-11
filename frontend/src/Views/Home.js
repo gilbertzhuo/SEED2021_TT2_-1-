269ab1ab5e6bc2e62862e29f8dac5a791587edd3
orelanthos32 <Snowy1080>
2020-12-11 15:21:37 +0800

    Login fixed

--- frontend/src/Views/Home.js
+++ frontend/src/Views/Home.js
@@ -1,87 +1,49 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAdjust,faBalanceScale,faCreditCard,faHandshake,faGift } from '@fortawesome/free-solid-svg-icons'
-function Home(){
-    return(
-            <div className="centre-align">
-                <h1 className="font-bold text-center">Welcome User1</h1>
-                <h2 className="font-bold text-center">  What do you want to do today?</h2>
-                <div className="bg-red-300">
-                        <ul>
-                        <li>
-                            <Link to="/Contact" 
-                            className="text-black p-3 border-t border-b block"
-                            >
-                                <span className = "text-xl">
-                                    <FontAwesomeIcon 
-                                        icon={faAdjust}
-                                    />
-                                </span>
-                                Contact Us
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/Transaction" 
-                            className="text-black p-3 border-t border-b block"
-                        >
-                                <span className = "text-xl">
-                                    <FontAwesomeIcon 
-                                        icon={faCreditCard}
-                                    />
-                                </span>
-                                View Transaction History
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/Transfer" 
-                            className="text-black p-3 border-t border-b block"
-                        >
-                                <span className = "text-xl">
-                                    <FontAwesomeIcon 
-                                        icon={faHandshake}
-                                    />
-                                </span>
-                                Make Payments
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/ViewBalance" 
-                            className="text-black p-3 border-t border-b block"
-                        >
-                                <span className = "text-xl">
-                                    <FontAwesomeIcon 
-                                        icon={faBalanceScale}
-                                    />
-                                </span>
-                                ViewBalance
-                            </Link>
-                        </li>
-                    </ul>
+//import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-                </div>
+import React from 'react';
+import Header from '../Components/Header'
+import Home from './Homepage'
+import About from './Transaction'
+import Contact from './ContactUs'
+import ApiProg from './Transfer'
+import ViewBalance from './ViewBalance';
+function App() {
+  return (
+      <div>
 
-                <div>
-                    <ul>
-                        <li>
-                            <Link to="/Rewards" 
-                            className="text-black p-3 border-t border-b"
-                            >
-                                <span className = "text-xl">
-                                    <FontAwesomeIcon 
-                                        icon={faGift}
-                                    />
-                                </span>
-                                Rewards
-                            </Link>
-                        </li>
-                        
-                    </ul>
+        <Router>
+          <Header/>
+          <div className="p-3">
+            <Switch>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route path="/Transaction">
+                <About/>
+              </Route>
+              <Route path="/Contact">
+                <Contact/>
+              </Route>
+              <Route path="/Transfer">
+                <ApiProg/>
+              </Route>
+              <Route path="/ViewBalance">
+                <ViewBalance/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
 
+      </div>
 
-                </div>
-                
-            </div>
-    )
+  );
+  //footer removed between the last DIV
 }
-export default Home
+
+export default App;
---
+++ frontend/src/Views/Homepage.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAdjust,faBalanceScale,faCreditCard,faHandshake } from '@fortawesome/free-solid-svg-icons'
+function Home(){
+    return(
+            <div className="centre-align">
+                <h1 className="font-bold text-center">Welcome User1</h1>
+
+                <h2 className="font-bold text-center">  What do you want to do today?</h2>
+                <div className="bg-red-300">
+                        <ul>
+                        <li>
+                            <Link to="/Contact" 
+                            className="text-black p-3 border-t border-b block"
+                            >
+                                <span className = "text-xl">
+                                    <FontAwesomeIcon 
+                                        icon={faAdjust}
+                                    />
+                                </span>
+                                Contact Us
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/Transaction" 
+                            className="text-black p-3 border-t border-b block"
+                        >
+                                <span className = "text-xl">
+                                    <FontAwesomeIcon 
+                                        icon={faCreditCard}
+                                    />
+                                </span>
+                                View Transaction History
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/Transfer" 
+                            className="text-black p-3 border-t border-b block"
+                        >
+                                <span className = "text-xl">
+                                    <FontAwesomeIcon 
+                                        icon={faHandshake}
+                                    />
+                                </span>
+                                Make Payments
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/ViewBalance" 
+                            className="text-black p-3 border-t border-b block"
+                        >
+                                <span className = "text-xl">
+                                    <FontAwesomeIcon 
+                                        icon={faBalanceScale}
+                                    />
+                                </span>
+                                ViewBalance
+                            </Link>
+                        </li>
+                    </ul>
+
+                </div>
+                
+            </div>
+    )
+}
+export default Home
