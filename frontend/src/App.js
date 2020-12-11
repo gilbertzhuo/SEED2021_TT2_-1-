b130072bbf907dd370f1018ef69b78220afeaa84
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-12-11 15:11:26 +0800

    Update request works but not add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 
 var axios = require('axios');
-var data = JSON.stringify({"custID":1});
+var data = JSON.stringify({"custID":1,"payeeID":1,"dateTime":new Date(2020,12,6),"amount":19,"expensesCat":"Shopping","eGift":false,"message":"hello there"});
 var config = {
   method: 'post',
   headers: { 
@@ -21,16 +21,21 @@ class App extends Component {
     accBal:null
   }
   
-  async postView(){
+  async postUpdate(){
     console.log("postView()...");
-    const res = await axios.post('https://cors-anywhere.herokuapp.com/https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view',data,config); //post(url,data)
-    const hold = Object.values(res.data);
-    console.log("hold arr: res.data",hold);
-
-    //this.setState({accounts:Object.values(hold[0])});
-    this.setState({accName:hold[0].accountName});
-    this.setState({accBal:hold[0].availableBal});
+    const res = await axios.post('https://cors-anywhere.herokuapp.com/https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/update',data,config); //post(url,data)
+    const hold = Object.values(res);
+    console.log("hold arr: res",hold);
   }
+
+  async postView(){ //postAdd
+    console.log("postView()...");
+    const res = await axios.post('https://cors-anywhere.herokuapp.com/https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/transaction/add',data,config); //post(url,data)
+    const hold = Object.values(res);
+    console.log("hold arr: res",hold);
+  }
+
+
 
   handleView = () =>{
     console.log("handleView() receives ",this.postView());
