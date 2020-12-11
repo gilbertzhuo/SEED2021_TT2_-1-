c3c5120fc6599fe9ac83c1c9be74f2a8e7bdebb5
Imaan Rowter <nadzir.4@gmail.com>
2020-12-11 13:20:14 +0800

    Contact Us page

--- frontend/src/Views/ContactUs.js
+++ frontend/src/Views/ContactUs.js
@@ -1,14 +1,47 @@
 import React from 'react'
-import CounterExample from '../Components/Counter_example'
+// import CounterExample from '../Components/Counter_example'
+import {Link} from "react-router-dom"
 
 function Contact(){
     return(
             <div>
                 <h1 className="font-bold">This is the Contact page</h1>
                 <h2>
-                    Our Github Link is:
+                    Our Github Link is: 
+                  
                 </h2>
-                <CounterExample/>
+                {/* <CounterExample/> */}
+
+                <a className="App-link" href="https://github.com/gilbertzhuo">
+                        Gilbert
+                </a>
+                <br/>
+                <a className="App-link" href="https://github.com/orelanthos32/">
+                        Bryan
+                </a>
+                <br/>
+                <a className="App-link" href="https://github.com/byc7">
+                        Yong Chun
+                </a>
+                <br/>
+                <a className="App-link" href="https://github.com/tjaslyn">
+                        Jaslyn
+                </a>
+                <br />
+
+                <a className="App-link" href="https://github.com/rowter4">
+                        Imaan
+                </a>
+                <br />
+                <ul>
+                    <li>
+                        <Link to="/" 
+                        className="text-blue-500 p-3 border-t border-b block">
+                            Back To Home
+                        </Link>
+                    </li>
+                </ul>    
+
             </div>
     )
 }
