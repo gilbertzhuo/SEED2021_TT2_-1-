f14ac52683d5214978dd889c264e7b886c1dc442
Imaan Rowter <nadzir.4@gmail.com>
2020-12-11 12:31:16 +0800

    payments routing

---
+++ frontend/src/Views/Payments.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+function Payments()  {
+
+    return(
+        <div>
+            <h1 className="font-bold">This is the Payments Page</h1>
+          
+        </div>
+    )
+
+
+}
+
+export default Payments
